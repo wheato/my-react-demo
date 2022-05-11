39e98dcd7fcb51212e440b6e11776eb640911452
wupengcheng <pengcheng.wu@shopee.com>
2022-05-11 15:21:08 +0800

    feat: add worker

--- pages/tables/worker.tsx
+++ pages/tables/worker.tsx
@@ -1,11 +1,36 @@
+import { Container, Typography } from '@mui/material';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { useEffect, useRef, useState } from 'react';
 
 const WorkerTable: NextPage = () => {
+  const tsWorkerRef = useRef<Worker | null>();
+  const [tsWorkerMessages, setTsWorkerMessages] = useState<String[]>([]);
+
+  useEffect(() => {
+    tsWorkerRef.current = new Worker(
+      new URL('../../workers/ts.worker.ts', import.meta.url)
+    );
+  
+    tsWorkerRef.current.addEventListener('message', (evt) => {
+      console.log('Message from TS worker:', evt.data);
+      const newMessages = [...tsWorkerMessages, evt.data];
+      setTsWorkerMessages(newMessages);
+    });
+
+    tsWorkerRef.current.postMessage({ type: 'start' });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>Worker Table</div>
+    <Container>
+      <Typography component="h2" variant="h2">Worker Table</Typography>
+      <Typography component="h3" variant="h3">TS worker messages:</Typography>
+      <pre>
+        {tsWorkerMessages
+          .map((msg) => JSON.stringify(msg, null, 2))
+          .join('\n\n')}
+      </pre>
+    </Container>
   );
 }
 export default WorkerTable
