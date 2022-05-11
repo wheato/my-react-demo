2d99bd45df1703fbe4e88f2a24303d33137efadc
wupengcheng <pengcheng.wu@shopee.com>
2022-05-11 18:50:04 +0800

    feat: add rust wasm worker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+
+const { resolve } = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    // Ensures that web workers can import scripts.
+    config.output.publicPath = '/_next/';
+
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
+
+    config.plugins.push(
+      new WasmPackPlugin({
+        crateDirectory: resolve('./rust'),
+        args: '--log-level warn',
+      })
+    );
+    
+    return config
+  },
 }
 
 module.exports = nextConfig
--- pages/tables/wasm.tsx
+++ pages/tables/wasm.tsx
@@ -1,11 +1,36 @@
+import { Container, Typography } from '@mui/material';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { useEffect, useRef, useState } from 'react';
 
 const WasmTable: NextPage = () => {
+  const wasmWorkerRef = useRef<Worker | null>();
+  const [wasmWorkerMessages, setWasmWorkerMessages] = useState<String[]>([]);
+
+  useEffect(() => {
+    wasmWorkerRef.current = new Worker(
+      new URL('../../workers/wasm.worker.ts', import.meta.url)
+    );
+  
+    wasmWorkerRef.current.addEventListener('message', (evt) => {
+      console.log('Message from TS worker:', evt.data);
+      const newMessages = [...wasmWorkerMessages, evt.data];
+      setWasmWorkerMessages(newMessages);
+    });
+
+    wasmWorkerRef.current.postMessage({ type: 'start' });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>WASM Table</div>
+    <Container>
+      <Typography component="h2" variant="h2">WebAssembly Table</Typography>
+      <Typography component="h3" variant="h3">Wasm worker messages:</Typography>
+      <pre>
+        {wasmWorkerMessages
+          .map((msg) => JSON.stringify(msg, null, 2))
+          .join('\n\n')}
+      </pre>
+    </Container>
   );
 }
 export default WasmTable
